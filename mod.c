c72a6f3ec72b4342d3ebe95f5e59abb9f14ff68a
nicksonpeter <nickrocky5200@gmail.com>
2023-05-10 05:51:11 -0700

    adding files

--- mod.c
+++ mod.c
@@ -1,17 +1,41 @@
 #include "monty.h"
 /**
-* free_stack - frees a doubly linked list
-* @head: head of the stack
+ * f_mod - computes the rest of the division of the second
+ * top element of the stack by the top element of the stack
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
 */
-void free_stack(stack_t *head)
+void f_mod(stack_t **head, unsigned int counter)
 {
-	stack_t *aux;
+	stack_t *h;
+	int len = 0, aux;
 
-	aux = head;
-	while (head)
+	h = *head;
+	while (h)
 	{
-		aux = head->next;
-		free(head);
-		head = aux;
+		h = h->next;
+		len++;
 	}
+	if (len < 2)
+	{
+		fprintf(stderr, "L%d: can't mod, stack too short\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	h = *head;
+	if (h->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	aux = h->next->n % h->n;
+	h->next->n = aux;
+	*head = h->next;
+	free(h);
 }
